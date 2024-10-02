8912c633381fbddb59f4c355ae44e12260adac01
Taaate <an981022@naver.com>
2024-10-02 21:55:42 +0900

    Initial Commit

---
+++ lecture-core/src/main/java/com/example/lecturecore/LectureCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.lecturecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LectureCoreApplication.class, args);
+    }
+
+}
---
+++ lecture-core/src/test/java/com/example/lecturecore/LectureCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lecturecore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
